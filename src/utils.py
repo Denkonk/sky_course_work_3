264e590c6e4d1ef86c85274f8664565252982b5b
Denis <ferz5566@yandex.ru>
2024-04-14 18:00:24 +0300

    add function get_data_trans(transactions)

--- src/utils.py
+++ src/utils.py
@@ -31,12 +31,30 @@ def get_first_5_trans(transactions):
     first_5_transactions = transactions[:5]
     return first_5_transactions
 
+def get_data_trans(transactions):
+    """
+    Перевода даты в формат ДД.ММ.ГГГГ
+    """
+    date_parts = transactions['date'].split('T')[0].split('-')
+    date_result = f"{date_parts[2]}.{date_parts[1]}.{date_parts[0]}."
+    return date_result
+
+
+
+
+
+# transaction_first_5 = get_first_5_trans(sorted_operations(filter_operations(load_json_data(file_path))))
+#
+# dates = [get_data_trans(transaction) for transaction in transaction_first_5]
+#
+# print(dates)
 
 
 
 #print(filter_operations(load_json_data(file_path)))
 #print(sorted_operations(filter_operations(load_json_data(file_path))))
 #print(get_first_5_trans(sorted_operations(filter_operations(load_json_data(file_path)))))
+#print(get_data_trans(get_first_5_trans(sorted_operations(filter_operations(load_json_data(file_path))))))
 #print(datetime.datetime.fromisoformat("2018-12-24T20:16:18.819037"))
 
 
