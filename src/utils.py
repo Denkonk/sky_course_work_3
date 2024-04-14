0a1a7d4e3fa76b13eaaa0e8e53b9a55d367751a8
Denis <ferz5566@yandex.ru>
2024-04-14 19:31:45 +0300

    add function mask_number_from_to(number_card)

--- src/utils.py
+++ src/utils.py
@@ -39,15 +39,42 @@ def get_data_trans(transactions):
     date_result = f"{date_parts[2]}.{date_parts[1]}.{date_parts[0]}."
     return date_result
 
+def mask_number_from_to(number_card):
+    """
+    Разделение и шифрование номера карты и счета
+    """
+    #number_card = 'счет 37664464522387841234'
+    #number_card = number_card[number]
+    last_space_index = number_card.rindex(' ')
+    first_part = number_card[:last_space_index]
+    second_part = number_card[last_space_index + 1:]
+    if len(second_part) == 16:
+        second_part_result = second_part[0:4] + ' ' + second_part[4:6] + '** **** ' + second_part[-4:]
+    elif len(second_part) == 20:
+        second_part_result = '**' + second_part[-4:]
+    else:
+        second_part_result = second_part
+    return f"{first_part} {second_part_result}"
 
 
 
 
-# transaction_first_5 = get_first_5_trans(sorted_operations(filter_operations(load_json_data(file_path))))
+
+
+
+
+transaction_first_5 = get_first_5_trans(sorted_operations(filter_operations(load_json_data(file_path))))
+
+print(transaction_first_5)
+
 #
 # dates = [get_data_trans(transaction) for transaction in transaction_first_5]
 #
 # print(dates)
+
+# for number_card in transaction_first_5:
+#     print(mask_number_from_to(number_card['from']))
+
 
 
 
